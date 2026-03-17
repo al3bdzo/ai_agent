9e2f7e2c7cd7010544e72a3948cf6957834b3d5e
al3bdzo <abdalrhmanmohammed307@gmail.com>
2026-03-18 00:02:16 +0200

    Setting up

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import argparse
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model = "gemini-2.5-flash",
+        contents = messages
+    )
+    return response
+
+def main():
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("No api key found!")
+
+    client = genai.Client(api_key=api_key)
+
+    parser = argparse.ArgumentParser(description="AI Agent CLI tool!")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    response = generate_content(client, messages)
+
+    if not response.usage_metadata:
+        raise RuntimeError("Failed API Request!")
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
